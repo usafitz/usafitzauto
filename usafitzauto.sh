de480e8a662122446f9e755da182412cdfe63bab
usafitz <github@usafitz.com>
2022-01-07 15:29:52 -0800

    syntax

--- usafitzauto.sh
+++ usafitzauto.sh
@@ -11,7 +11,7 @@ echo "3 GOBUSTER "
 echo "       "
 read -p "YOUR SELECTION: " answer
 
-if [[ $answer = 1 ]] ; then
+if [[ $answer = 1 ]] then
 # NMAP
     echo "You have selected to use NMAP"
     echo "Which type of scan would you like?"
@@ -19,12 +19,12 @@ if [[ $answer = 1 ]] ; then
     echo "2 High Enumeration"
     read -p "YOUR SELECTION:  " namppreference
     echo "  "
-        if [[ $nmappreference = 1]] ; then
+        if [[ $nmappreference = 1]] then
             echo "BEGIN COMMON NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
             nmap -A -T4 -p- $ip
             echo " "
             echo "END COMMON NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
-        elif [[ $nmappreference = 2]] ; then
+        elif [[ $nmappreference = 2]] then
             echo "BEGIN VULN NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
             nmap -sV -vv --script vuln $ip
             echo "  "
