7c15f14ac34ee76a6598654840961ebf4b214e05
usafitz <github@usafitz.com>
2022-01-07 15:34:16 -0800

    syntax

--- usafitzauto.sh
+++ usafitzauto.sh
@@ -11,27 +11,28 @@ echo "3 GOBUSTER "
 echo "       "
 read -p "YOUR SELECTION: " answer
 
-if [[ $answer = 1 ]] then
-# NMAP
-    echo "You have selected to use NMAP"
-    echo "Which type of scan would you like?"
-    echo "1 Common & Popular"
-    echo "2 High Enumeration"
-    read -p "YOUR SELECTION:  " namppreference
-    echo "  "
-        if [[ $nmappreference = 1]] then
-            echo "BEGIN COMMON NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
-            nmap -A -T4 -p- $ip
-            echo " "
-            echo "END COMMON NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
-        elif [[ $nmappreference = 2]] then
-            echo "BEGIN VULN NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
-            nmap -sV -vv --script vuln $ip
-            echo "  "
-            echo "END VULN NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
-        else   
-            exit 1
-        fi
+if [[ $answer = 1 ]] 
+    then
+    # NMAP
+        echo "You have selected to use NMAP"
+        echo "Which type of scan would you like?"
+        echo "1 Common & Popular"
+        echo "2 High Enumeration"
+        read -p "YOUR SELECTION:  " namppreference
+        echo "  "
+            if [[ $nmappreference = 1]] then
+                echo "BEGIN COMMON NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
+                nmap -A -T4 -p- $ip
+                echo " "
+                echo "END COMMON NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
+            elif [[ $nmappreference = 2]] then
+                echo "BEGIN VULN NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
+                nmap -sV -vv --script vuln $ip
+                echo "  "
+                echo "END VULN NMAP SCAN OF $ip -- {[$DAY] - $DATE}"
+            else   
+                exit 1
+            fi
 
 elif [[ $answer = 2 ]] ; then
 # DIRB
