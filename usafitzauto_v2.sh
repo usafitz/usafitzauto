ef6285039e13eca85a5d6746bae432f1116ab74b
usafitz <github@usafitz.com>
2022-01-13 11:50:24 -0800

    move output_files

--- usafitzauto_v2.sh
+++ usafitzauto_v2.sh
@@ -3,38 +3,60 @@
 DAY=$(date +%u)
 DATE=$(date +%Y%b%d-%T)
 
-read -p "Which IP Address?  " ip
+read -p "Which IP or NETWORK [10.11.1.0/24]:  " ip
 echo "  "
 
 function quit {
 	exit
 }
 
-function nmap_sn {
+function changefilename {
+    
+}
+
+function nmap_sn { # SCAN NETWORK FOR HOSTS THAT ARE UP
 	echo "  "
 	echo "==================="
 	echo "=== HOSTS FOUND ==="
 	echo "==================="
-	nmap -sn $ip | grep "report for" | cut -d " " -f 5 >> hostsup_$DATE.txt
+	nmap -sn $ip | grep "report for" | cut -d " " -f 5 >> ./output_files/$DATE/hostsup_$DATE.txt
 	cat hostsup_$DATE.txt
 	#nmap -sn $1 -oG nmap_$1_fullrange
 	echo "==================="
 }
 
-function nmap_sl {
+function nmap_sl { # FUTURE POSSIBILITIES
 	nmap -sL -oG - -iR 5 $ip
 }
 
-function nmap_command {
-	sudo nmap -sS $1 -oG $1
+function nmap_command { # TCP SYN SCAN (REQUIRES SUDO | QUICK AND EASY)
+	sudo nmap -A -sS $1 -oG ./output_files/$DATE/$1 >> ./output_files/$DATE/$1_test.txt
 }
 
+# CREATE FRESH DIRECTORY FOR ALL OUTPUT FILES
+mkdir ./output_files/$DATE 
+
+# DO AN INITIAL SCAN TO SEE WHICH IP IS AVAILABLE
 nmap_sn $ip
 
+# USE THE CREATED LIST TO SCAN INDIVIDUAL COMPUTERS
 for p in $(cat hostsup_$DATE.txt)
 do
 	nmap_command $p
+
+    echo "  "
+
+    string='My long string'
+    if [[ $string == *"My long"* ]]; then
+        echo "It's there!"
+    fi
+    # cat output_files/$DATE/$1 | grep "Windows" 
 done
+
+
+
+
+
 
 #read -p "Which IP or Network?:  " ip
 #echo "  "
